d50a0810e469ede6e51f14a1f410a57e081254b9
declick <et.miracle@hotmail.fr>
2023-08-03 22:25:00 +0200

    seconde commit

--- git.py
+++ git.py
@@ -1,13 +1,29 @@
-import os , random
+import os
+from datetime import datetime, timedelta
 
-for i in range(200):
-    d = str(i) + 'days ago'
-    rand = random.randrange(1, 12)
-    with open('test.txt','a') as file:
-        file.write(d+'\n')
+def get_all_dates_in_year(year):
+    start_date = datetime(year, 1, 1)
+    end_date = datetime(year, 12, 31)
+    delta = timedelta(days=1)
+    current_date = start_date
+    all_dates = []
+    while current_date <= end_date:
+        all_dates.append(current_date.strftime('%Y-%m-%d'))
+        current_date += delta
+    return all_dates
+
+year = 2021
+all_dates = get_all_dates_in_year(year)
+
+for i, d in enumerate(all_dates):
+    with open('test.txt', 'a') as file:
+        file.write(d + '\n')
     os.system('git add test.txt')
-    os.system('git commit --date=" 2021-'+str(rand)+'-'+d+'" -m 1')
+    os.system(f'git commit --date="{d}" -m "Commit {i+1}"')
+
 os.system('git push -u origin main')
+
+
 
 #git commit --amend --no-edit --date="Fri Nov 6 20:00:00 2015 -0600" 
 #git fetch origin 
